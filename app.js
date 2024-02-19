464d826135aba6f2cffedc12a201562ba09948b9
ziani96 <81708016+ziani96@users.noreply.github.com>
2024-02-19 14:29:24 +0000

    get: /api endpoint

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require("express");
-const { getTopics } = require("./app-controllers/controllers");
+
+const { getEndPoints } = require("./app-controllers/api-controllers");
+const { getTopics } = require("./app-controllers/topics-controllers");
 const app = express();
- 
-app.get("/api/topics" , getTopics)
 
-app.all('/*', (request,response,next) => {
-  response.status(404).send({msg: "path not found"})
+app.get("/api/topics", getTopics);
 
-})
+app.get("/api", getEndPoints);
 
+app.all("/*", (request, response, next) => {
+  response.status(404).send({ msg: "path not found" });
+});
 
-
-module.exports = app
+module.exports = app;
