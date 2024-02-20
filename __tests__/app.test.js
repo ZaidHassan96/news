40ee37e40477a41f08a7ee7decdb6b140d684a7c
ziani96 <81708016+ziani96@users.noreply.github.com>
2024-02-20 12:04:47 +0000

    added GET /api/articles

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,7 @@ const seed = require("../db/seeds/seed");
 const testData = require("../db/data/test-data/index");
 const app = require("../app");
 const endPoints = require("../endpoints.json");
+const { string } = require("pg-format");
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
@@ -50,16 +51,18 @@ describe("GET: /api/articles/:article_id", () => {
       .get("/api/articles/3")
       .expect(200)
       .then((response) => {
-        expect(response.body.article).toMatchObject({
-          article_id: 3,
-          title: "Eight pug gifs that remind me of mitch",
-          topic: "mitch",
-          author: "icellusedkars",
-          body: "some gifs",
-          created_at: "2020-11-03T09:12:00.000Z",
-          votes: 0,
-          article_img_url:
-            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+        expect(response.body).toMatchObject({
+          article: {
+            article_id: 3,
+            title: "Eight pug gifs that remind me of mitch",
+            topic: "mitch",
+            author: "icellusedkars",
+            body: "some gifs",
+            created_at: "2020-11-03T09:12:00.000Z",
+            votes: 0,
+            article_img_url:
+              "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+          },
         });
       });
   });
@@ -78,5 +81,90 @@ describe("GET: /api/articles/:article_id", () => {
       .then((response) => {
         expect(response.body.msg).toBe("Bad request");
       });
+  });
+});
+describe("GET: /api/articles", () => {
+  test("returns an article array of article objects with a 200 status code", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then((response) => {
+        const articles = response.body.articles;
+        expect(articles).toHaveLength(13);
+      });
+  });
+  test("returns all articles with these properties: author, title, article_id, topic ,created_at, votes, article_img_url, comment_count sorted by date in descending order", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then((response) => {
+        const articles = response.body.articles;
+        articles.forEach((article) => {
+          expect(article).toMatchObject({
+            article_id: expect.any(Number),
+            title: expect.any(String),
+            topic: expect.any(String),
+            author: expect.any(String),
+            created_at: expect.any(String),
+            votes: expect.any(Number),
+            article_img_url: expect.any(String),
+            comment_count: expect.any(Number),
+          });
+        });
+      });
+  });
+  describe("article queries", () => {
+    test("should return the articles sorted by authors", () => {
+      return request(app)
+        .get("/api/articles?sort_by=author")
+        .expect(200)
+        .then((response) => {
+          const articles = response.body.articles;
+          expect(articles).toBeSortedBy("author");
+        });
+    });
+    test("should return the articles sorted by topics", () => {
+      return request(app)
+        .get("/api/articles?sort_by=topic")
+        .expect(200)
+        .then((response) => {
+          const articles = response.body.articles;
+          expect(articles).toBeSortedBy("topic");
+        });
+    });
+    test("should return the articles sorted by authors either asc or desc", () => {
+      return request(app)
+        .get("/api/articles?sort_by=author&&order=asc")
+        .expect(200)
+        .then((response) => {
+          const articles = response.body.articles;
+          expect(articles).toBeSortedBy("author");
+        });
+    });
+    test("should return the articles sorted by topics either asc or desc", () => {
+      return request(app)
+        .get("/api/articles?sort_by=topic&&order=desc")
+        .expect(200)
+        .then((response) => {
+          const articles = response.body.articles;
+          expect(articles).toBeSortedBy("topic", { descending: true });
+        });
+    });
+    test("should return 400 bad request if an invalid query is entered", () => {
+      return request(app)
+        .get("/api/articles?sort_by=banana")
+        .expect(400)
+        .then((response) => {
+          expect(response.body.msg).toBe("Invalid query");
+        });
+    });
+    test("should return 400 bad request if an invalid order query is entered", () => {
+      return request(app)
+        .get("/api/articles?order=banana")
+        .expect(400)
+        .then((response) => {
+          expect(response.body.msg).toBe("Invalid query");
+        });
+    });
   });
 });
